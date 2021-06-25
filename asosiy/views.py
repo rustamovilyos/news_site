e811c09f95695d609fdfaf65ad4619ff6a03d8be
rustamovilyos <rustamov7871@gmail.com>
2021-06-25 19:18:40 +0500

    new version

--- asosiy/views.py
+++ asosiy/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView, DetailView
 
 
 def index(request):
-    maqolalar = Article.objects.all()[:4]
+    maqolalar = Article.objects.all()[:3]
     kategoriyalar = Category.objects.all()
     return render(request, 'asosiy/index.html', {'turi': kategoriyalar, 'maqolalar': maqolalar})
 
@@ -16,7 +16,7 @@ def asosiy(request, asosiy_id):
     tip = Category.objects.get(pk=asosiy_id)
     kategoriyalar = Category.objects.all()
     all_maqolalar = Article.objects.filter(toifa=tip)
-    paginator = Paginator(all_maqolalar, 3)
+    paginator = Paginator(all_maqolalar, 2)
     maqolalar = paginator.get_page(page)
 
     return render(request, 'asosiy/index.html', {'turi': kategoriyalar, 'maqolalar': maqolalar})
@@ -25,3 +25,8 @@ def asosiy(request, asosiy_id):
 class ArticleView(DetailView):
     model = Article
     template_name = 'asosiy/articleView.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['turi'] = Category.objects.all()
+        return context
